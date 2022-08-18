b051b3fc4cabf8d92e0543d2c41072db6ae9e994
pjIdM <69456861+PiDGMT@users.noreply.github.com>
2022-08-18 12:06:26 +0200

    correct weights

--- models/triplet.py
+++ models/triplet.py
@@ -20,7 +20,7 @@ class Tripletb0(torch.nn.Module):
 if __name__ == "__main__":
   net = Tripletb0().cuda()
   #hieronder zijn de weights
-  checkpoint = torch.load('../weights/triplet1501b0')
+  checkpoint = torch.load('../weights/triplet1501V2validb1schedule')
   net.load_state_dict(checkpoint['model_state_dict'])
   #dit is om hem in evaluation modus te zetten
   net.eval()
