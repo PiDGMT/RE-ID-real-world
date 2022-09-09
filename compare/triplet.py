b6ff45496f7d2d7b8e564b554efdc51b3b80228a
pjIdM <69456861+PiDGMT@users.noreply.github.com>
2022-09-09 14:59:25 +0200

    update

--- compare/triplet.py
+++ compare/triplet.py
@@ -2,12 +2,20 @@ from torchvision import models
 import torch
 
 class Tripletb0(torch.nn.Module):
-
+   
     def __init__(self):
+         """
+         Loads the model achitecture and the pretrained weights
+         """
         super(Tripletb0, self).__init__()
         self.net = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights)
 
     def forward(self, input1, input2, input3=None):
+        """
+        Forwards the inpputs trough the model 
+        Input3 = None, as this inpput is not used during testing
+        returns the outputs
+        """
 
         output1 = self.net(input1)
         output2 = self.net(input2)
@@ -20,7 +28,7 @@ class Tripletb0(torch.nn.Module):
 if __name__ == "__main__":
   net = Tripletb0().cuda()
   #hieronder zijn de weights
-  checkpoint = torch.load('C:/Users/piete/Documents/siamese/weights/Triplet1501V2extra30')
+  checkpoint = torch.load('C:/Users/piete/Documents/siamese/weights/triplet1501V2validb0schedule'')
   net.load_state_dict(checkpoint['model_state_dict'])
   #dit is om hem in evaluation modus te zetten
   net.eval()
