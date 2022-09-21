34a30cff3eee06eb4c5e624c3848d43486c45fbe
pjIdM <69456861+PiDGMT@users.noreply.github.com>
2022-09-21 15:28:24 +0200

    Description now correct

--- test_models/score.py
+++ test_models/score.py
@@ -6,8 +6,8 @@ def true_target_rate(Nt2t, Nt):
     """
     True Target Rate
 
-    Nt2t: the number of correctly verified probe sets of target people
-    Nt: number of probe sets from target people
+    Nt2t: the number of correctly verified gallery sets of probe people
+    Nt: number of gallery sets from target people
     """
     return Nt2t / Nt
 
@@ -15,8 +15,8 @@ def false_target_rate(Nnt2t, Nnt):
     """
     False Target Rate
 
-    Nn2t: number of probe sets of non-targets but verified as target people
-    Nnt: number of probe sets from non-target people
+    Nn2t: number of gallery sets of non-targets but verified as probe people
+    Nnt: number of gallery sets from non-target people
 
     """
     return Nnt2t / Nnt
